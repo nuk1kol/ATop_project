a500bc54faa3cc8ec3274fdc417e85c820f87a3b
nur1kol <kolomiets.n@mail.ru>
2024-11-15 09:52:51 +0300

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,6 +1,6 @@
 import telebot
 
-API_TOKEN = '7701242307:AAGCBbbKdUthratz6K8chD1rc_gMsY9C5kw'
+API_TOKEN = 'ххххххххххххххххххххххххххх'
 
 # Создаем и настраиваем объект бота
 bot = telebot.TeleBot(API_TOKEN)
